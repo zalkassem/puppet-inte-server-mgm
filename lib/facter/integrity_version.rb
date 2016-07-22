89a38630c2c831213aa4af696e738f6efb016374
Lennart Betz <lennart.betz@netways.de>
2016-07-22 14:21:11 +0200

    add fact for installed version

---
+++ lib/facter/integrity_version.rb
@@ -0,0 +1,9 @@
+Facter.add(:integrity_version) do
+  setcode do
+    server_version = 'unknown'
+    if output = Facter::Util::Resolution.exec('rpm -q --queryformat "%{Version}" ptc-integrity-server')
+      server_version = output if /^[0-9]+.[0-9]+$/.match(output)
+    end
+    server_version
+  end
+end
